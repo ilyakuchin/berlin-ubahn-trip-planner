9920c054d60edfc26e1c5c1ee8dc258afe890136
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-04-30 01:13:57 +0300

    Create Lines and Stations folders, Add points to U1 line

---
+++ src/Lines/U1Lines.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+export default function U1Lines() {
+  return (
+    <>
+      <polyline
+        fill='none'
+        stroke='#53A947'
+        stroke-width='8'
+        stroke-miterlimit='10'
+        points='786.015,1143.516 1931.229,1143.516 
+	1992.727,1082.02 '
+      />
+      <circle
+        fill='#FFFFFF'
+        stroke='#000000'
+        stroke-width='2'
+        stroke-miterlimit='10'
+        cx='784.695'
+        cy='1143.516'
+        r='8.5'
+      />
+      <circle
+        fill='#FFFFFF'
+        stroke='#000000'
+        stroke-width='2'
+        stroke-miterlimit='10'
+        cx='1189.695'
+        cy='1143.516'
+        r='8.5'
+      />
+      <circle
+        fill='#FFFFFF'
+        stroke='#000000'
+        stroke-width='2'
+        stroke-miterlimit='10'
+        cx='1648.301'
+        cy='1143.516'
+        r='8.5'
+      />
+      <circle
+        fill='#FFFFFF'
+        stroke='#000000'
+        stroke-width='2'
+        stroke-miterlimit='10'
+        cx='1825.104'
+        cy='1143.516'
+        r='8.5'
+      />
+      <circle
+        fill='#FFFFFF'
+        stroke='#000000'
+        stroke-width='2'
+        stroke-miterlimit='10'
+        cx='1918.928'
+        cy='1143.516'
+        r='8.5'
+      />
+      <circle
+        fill='#FFFFFF'
+        stroke='#000000'
+        stroke-width='2'
+        stroke-miterlimit='10'
+        cx='1991.5065'
+        cy='1080.7995'
+        r='8.5'
+      />
+    </>
+  );
+}
--- src/Lines/U2Lines.js
+++ src/Lines/U2Lines.js
@@ -3,6 +3,15 @@ import React from 'react';
 export default function U2Lines() {
   return (
     <>
+      <polyline
+        fill='none'
+        stroke='#E02021'
+        stroke-width='8'
+        stroke-miterlimit='10'
+        points='1728.824,278.344 1728.824,958.396 
+	1668.721,1018.5 1308.72,1018.805 1308.72,1098.805 1238.01,1169.516 987.695,1169.516 783.5,965.239 431,965.239 324.934,859.173 
+	'
+      />
       <line
         fill='none'
         stroke='#E02021'
@@ -12,15 +21,6 @@ export default function U2Lines() {
         y1='1033.5'
         x2='1293.72'
         y2='1033.5'
-      />
-      <polyline
-        fill='none'
-        stroke='#E02021'
-        stroke-width='8'
-        stroke-miterlimit='10'
-        points='1728.824,278.344 1728.824,958.396 
-	1668.721,1018.5 1308.72,1018.805 1308.72,1098.805 1238.01,1169.516 987.695,1169.516 783.5,965.239 431,965.239 324.934,859.173 
-	'
       />
       <path
         fill='none'
