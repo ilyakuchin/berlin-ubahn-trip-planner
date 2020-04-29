ef74001719db556007f64ab160ec58de30fef3e0
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-04-29 12:27:51 +0300

    Extract legend

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import StationLabel from './StationLabel';
+import Legend from './Legend';
 const stationLabelArr = require('./labels.json');
 
 export default function Map() {
@@ -3020,198 +3021,6 @@ export default function Map() {
         x2='1495.891'
         y2='786.231'
       />
-      <rect
-        x='2419.881'
-        y='65.655'
-        fill='none'
-        stroke='#000000'
-        stroke-width='2'
-        stroke-miterlimit='10'
-        width='251.119'
-        height='364.346'
-      />
-      <text
-        transform='matrix(1 0 0 1 2474.7891 94.6213)'
-        font-family="'TrebuchetMS'"
-        font-size='24'
-      >
-        Legend
-      </text>
-      <line
-        fill='none'
-        stroke='#53A947'
-        stroke-width='8'
-        stroke-miterlimit='10'
-        x1='2453.5'
-        y1='115'
-        x2='2514.5'
-        y2='115'
-      />
-      <text
-        transform='matrix(1 0 0 1 2544.5 121.0002)'
-        font-family="'TrebuchetMS'"
-        font-size='24'
-      >
-        U1
-      </text>
-      <line
-        fill='none'
-        stroke='#E02021'
-        stroke-width='8'
-        stroke-miterlimit='10'
-        x1='2453.5'
-        y1='145'
-        x2='2514.5'
-        y2='145'
-      />
-      <text
-        transform='matrix(1 0 0 1 2544.5 151.0002)'
-        font-family="'TrebuchetMS'"
-        font-size='24'
-      >
-        U2
-      </text>
-      <line
-        fill='none'
-        stroke='#339393'
-        stroke-width='8'
-        stroke-miterlimit='10'
-        x1='2453.5'
-        y1='175'
-        x2='2514.5'
-        y2='175'
-      />
-      <text
-        transform='matrix(1 0 0 1 2544.5 181.0002)'
-        font-family="'TrebuchetMS'"
-        font-size='24'
-      >
-        U3
-      </text>
-      <line
-        fill='none'
-        stroke='#F5D021'
-        stroke-width='8'
-        stroke-miterlimit='10'
-        x1='2453.5'
-        y1='205'
-        x2='2514.5'
-        y2='205'
-      />
-      <text
-        transform='matrix(1 0 0 1 2544.5 211.0002)'
-        font-family="'TrebuchetMS'"
-        font-size='24'
-      >
-        U4
-      </text>
-      <line
-        fill='none'
-        stroke='#89592D'
-        stroke-width='8'
-        stroke-miterlimit='10'
-        x1='2453.5'
-        y1='235'
-        x2='2514.5'
-        y2='235'
-      />
-      <text
-        transform='matrix(1 0 0 1 2544.5 241.0002)'
-        font-family="'TrebuchetMS'"
-        font-size='24'
-      >
-        U5/U55
-      </text>
-      <line
-        fill='none'
-        stroke='#7C6CA4'
-        stroke-width='8'
-        stroke-miterlimit='10'
-        x1='2453.5'
-        y1='265'
-        x2='2514.5'
-        y2='265'
-      />
-      <text
-        transform='matrix(1 0 0 1 2544.5 271.0002)'
-        font-family="'TrebuchetMS'"
-        font-size='24'
-      >
-        U6
-      </text>
-      <line
-        fill='none'
-        stroke='#1793C5'
-        stroke-width='8'
-        stroke-miterlimit='10'
-        x1='2453.5'
-        y1='295'
-        x2='2514.5'
-        y2='295'
-      />
-      <text
-        transform='matrix(1 0 0 1 2544.5 301.0002)'
-        font-family="'TrebuchetMS'"
-        font-size='24'
-      >
-        U7
-      </text>
-      <line
-        fill='none'
-        stroke='#035D96'
-        stroke-width='8'
-        stroke-miterlimit='10'
-        x1='2453.5'
-        y1='325'
-        x2='2514.5'
-        y2='325'
-      />
-      <text
-        transform='matrix(1 0 0 1 2544.5 331.0002)'
-        font-family="'TrebuchetMS'"
-        font-size='24'
-      >
-        U8
-      </text>
-      <line
-        fill='none'
-        stroke='#EC651A'
-        stroke-width='8'
-        stroke-miterlimit='10'
-        x1='2453.5'
-        y1='355'
-        x2='2514.5'
-        y2='355'
-      />
-      <text
-        transform='matrix(1 0 0 1 2544.5 361.0002)'
-        font-family="'TrebuchetMS'"
-        font-size='24'
-      >
-        U9
-      </text>
-      <text
-        transform='matrix(1 0 0 1 2544.5 391.0002)'
-        font-family="'TrebuchetMS'"
-        font-size='18'
-      >
-        Austausch mit
-      </text>
-      <text
-        transform='matrix(1 0 0 1 2544.5 412.5999)'
-        font-family="'TrebuchetMS'"
-        font-size='18'
-      >
-        die S-bahn
-      </text>
-      <text
-        transform='matrix(1 0 0 1 1485.5 866.0002)'
-        fill='#AAAAAA'
-        font-family="'TrebuchetMS'"
-        font-size='14'
-      >
-        (Dieser Abschnitt ist im Bau)
-      </text>
       <line
         fill='none'
         stroke='#7C6CA4'
@@ -3269,6 +3078,7 @@ export default function Map() {
         x2='1348.721'
         y2='1002.5'
       />
+      <Legend />
     </svg>
   );
 }
