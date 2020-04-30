05ed97cb8c43b7c39606b612b878186c97ddf169
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-04-30 11:27:57 +0300

    Add station points to Line U4

--- src/Lines/U4Lines.js
+++ src/Lines/U4Lines.js
@@ -11,35 +11,32 @@ export default function U4Lines() {
         points='1097.695,1182.516 1037.695,1182.516 
 	1037.695,1447.031 '
       />
-      <line
-        fill='none'
-        stroke='#F5D021'
-        stroke-width='8'
+      <circle
+        fill='#FFFFFF'
+        stroke='#000000'
+        stroke-width='2'
         stroke-miterlimit='10'
-        x1='1037.695'
-        y1='1263.332'
-        x2='1052.695'
-        y2='1263.332'
+        cx='1037.695'
+        cy='1263.332'
+        r='8.5'
       />
-      <line
-        fill='none'
-        stroke='#F5D021'
-        stroke-width='8'
+      <circle
+        fill='#FFFFFF'
+        stroke='#000000'
+        stroke-width='2'
         stroke-miterlimit='10'
-        x1='1022.695'
-        y1='1401.998'
-        x2='1037.695'
-        y2='1401.998'
+        cx='1037.695'
+        cy='1401.998'
+        r='8.5'
       />
-      <line
-        fill='none'
-        stroke='#F5D021'
-        stroke-width='8'
+      <circle
+        fill='#FFFFFF'
+        stroke='#000000'
+        stroke-width='2'
         stroke-miterlimit='10'
-        x1='1022.695'
-        y1='1447.043'
-        x2='1052.695'
-        y2='1447.043'
+        cx='1037.695'
+        cy='1447.043'
+        r='8.5'
       />
     </>
   );
