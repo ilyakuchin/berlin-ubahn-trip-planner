f700171e79445bc2293fa6d7c6880565be4188c6
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-04-30 19:53:20 +0300

    Extracted U1,U2 stations data to array of objects

--- src/Lines/U1Lines.js
+++ src/Lines/U1Lines.js
@@ -1,6 +1,45 @@
 import React from "react";
 import StationPoint from "../StationPoint";
 export default function U1Lines() {
+	const data = [
+		{
+			cx: "784.695",
+			cy: "1143.516",
+			label: "Uhland-straße"
+		},
+		{
+			cx: "1189.695",
+			cy: "1143.516",
+			label: "Kurfürstenstraße"
+		},
+		{
+			cx: "1648.301",
+			cy: "1143.516",
+			label: "Prinzenstraße"
+		},
+		{
+			cx: "1825.104",
+			cy: "1143.516",
+			label: "Görlitzer Bahnhof"
+		},
+		{
+			cx: "1918.928",
+			cy: "1143.516",
+			label: "Schlesisches Tor"
+		},
+		{
+			cx: "1991.5065",
+			cy: "1080.7995",
+			label: "Warschauer Straße"
+		}
+	];
+	const stations = data.map(station => (
+		<StationPoint
+			cx={station.cx}
+			cy={station.cy}
+			label={station.label}
+		/>
+	));
 	return (
 		<>
 			<polyline
@@ -11,36 +50,7 @@ export default function U1Lines() {
 				points="786.015,1143.516 1931.229,1143.516 
 	1992.727,1082.02 "
 			/>
-			<StationPoint
-				cx="784.695"
-				cy="1143.516"
-				label="Uhland-straße"
-			/>
-			<StationPoint
-				cx="1189.695"
-				cy="1143.516"
-				label="Kurfürstenstraße"
-			/>
-			<StationPoint
-				cx="1648.301"
-				cy="1143.516"
-				label="Prinzenstraße"
-			/>
-			<StationPoint
-				cx="1825.104"
-				cy="1143.516"
-				label="Görlitzer Bahnhof"
-			/>
-			<StationPoint
-				cx="1918.928"
-				cy="1143.516"
-				label="Schlesisches Tor"
-			/>
-			<StationPoint
-				cx="1991.5065"
-				cy="1080.7995"
-				label="Warschauer Straße"
-			/>
+			{stations}
 		</>
 	);
 }
--- src/Lines/U2Lines.js
+++ src/Lines/U2Lines.js
@@ -2,6 +2,126 @@ import React from "react";
 import StationPoint from "../StationPoint";
 
 export default function U2Lines() {
+	const data = [
+		{
+			cx: "1308.72",
+			cy: "1033.5",
+			label: "Mendelssohn-Bartholdz-Park"
+		},
+		{
+			cx: "1190",
+			cy: "1169.516",
+			label: "Bülowstraße"
+		},
+		{
+			cx: "1443.721",
+			cy: "1018.5",
+			label: "Mohrenstraße"
+		},
+		{
+			cx: "1591.498",
+			cy: "1018.5",
+			label: "Hausvogteiplatz"
+		},
+		{
+			cx: "1651.498",
+			cy: "1018.5",
+			label: "Spittelmarkt"
+		},
+		{
+			cx: "1678.075",
+			cy: "1009.146",
+			label: "Märkisches Museum"
+		},
+		{
+			cx: "1720.502",
+			cy: "966.719",
+			label: "Klosterstraße"
+		},
+		{
+			cx: "1728.824",
+			cy: "723.5",
+			label: "Rosa-Luxemburg-Platz"
+		},
+		{
+			cx: "1728.824",
+			cy: "668.5",
+			label: "Senefeldplatz"
+		},
+		{
+			cx: "1728.824",
+			cy: "613.5",
+			label: "Eberswalder Straße"
+		},
+		{
+			cx: "1728.824",
+			cy: "433.5",
+			label: "Vinetastraße"
+		},
+		{
+			cx: "808.61",
+			cy: "990.431",
+			label: "Ernst-Reuter-Platz"
+		},
+		{
+			cx: "750.918",
+			cy: "965.239",
+			label: "Deutsche Oper"
+		},
+		{
+			cx: "617.105",
+			cy: "965.239",
+			label: "Sophie-Charlotte-Platz"
+		},
+		{
+			cx: "445.918",
+			cy: "965.239",
+			label: "Theodor-Heuss-Platz"
+		},
+		{
+			cx: "395.645",
+			cy: "929.884",
+			label: "Neu-Westend"
+		},
+		{
+			cx: "360.289",
+			cy: "894.528",
+			label: "Olympiastadion"
+		},
+		{
+			cx: "324.9335",
+			cy: "859.1725",
+			label: "Ruhleben"
+		},
+		{
+			cx: "536.533",
+			cy: "967.067",
+			label: "Kaiserdamm"
+		},
+		{
+			cx: "1728.824",
+			cy: "559",
+			label: "Schönhauser Allee"
+		},
+		{
+			cx: "1728.824",
+			cy: "278.344",
+			label: "Pankow"
+		},
+		{
+			cx: "1348.721",
+			cy: "1017.5",
+			label: "Potsdamer Platz"
+		}
+	];
+	const stations = data.map(station => (
+		<StationPoint
+			cx={station.cx}
+			cy={station.cy}
+			label={station.label}
+		/>
+	));
+
 	return (
 		<>
 			<polyline
@@ -13,116 +133,7 @@ export default function U2Lines() {
 	1668.721,1018.5 1308.72,1018.805 1308.72,1098.805 1238.01,1169.516 987.695,1169.516 783.5,965.239 431,965.239 324.934,859.173 
 	"
 			/>
-			<StationPoint
-				cx="1308.72"
-				cy="1033.5"
-				label="Mendelssohn-Bartholdz-Park"
-			/>
-			<StationPoint
-				cx="1190"
-				cy="1169.516"
-				label="Bülowstraße"
-			/>
-			<StationPoint
-				cx="1443.721"
-				cy="1018.5"
-				label="Mohrenstraße"
-			/>
-			<StationPoint
-				cx="1591.498"
-				cy="1018.5"
-				label="Hausvogteiplatz"
-			/>
-			<StationPoint
-				cx="1651.498"
-				cy="1018.5"
-				label="Spittelmarkt"
-			/>
-			<StationPoint
-				cx="1678.075"
-				cy="1009.146"
-				label="Märkisches Museum"
-			/>
-			<StationPoint
-				cx="1720.502"
-				cy="966.719"
-				label="Klosterstraße"
-			/>
-			<StationPoint
-				cx="1728.824"
-				cy="723.5"
-				label="Rosa-Luxemburg-Platz"
-			/>
-			<StationPoint
-				cx="1728.824"
-				cy="668.5"
-				label="Senefeldplatz"
-			/>
-			<StationPoint
-				cx="1728.824"
-				cy="613.5"
-				label="Eberswalder Straße"
-			/>
-			<StationPoint
-				cx="1728.824"
-				cy="433.5"
-				label="Vinetastraße"
-			/>
-			<StationPoint
-				cx="808.61"
-				cy="990.431"
-				label="Ernst-Reuter-Platz"
-			/>
-			<StationPoint
-				cx="750.918"
-				cy="965.239"
-				label="Deutsche Oper"
-			/>
-			<StationPoint
-				cx="617.105"
-				cy="965.239"
-				label="Sophie-Charlotte-Platz"
-			/>
-			<StationPoint
-				cx="445.918"
-				cy="965.239"
-				label="Theodor-Heuss-Platz"
-			/>
-			<StationPoint
-				cx="395.645"
-				cy="929.884"
-				label="Neu-Westend"
-			/>
-			<StationPoint
-				cx="360.289"
-				cy="894.528"
-				label="Olympiastadion"
-			/>
-			<StationPoint
-				cx="324.9335"
-				cy="859.1725"
-				label="Ruhleben"
-			/>
-			<StationPoint
-				cx="536.533"
-				cy="967.067"
-				label="Kaiserdamm"
-			/>
-			<StationPoint
-				cx="1728.824"
-				cy="559"
-				label="Schönhauser Allee"
-			/>
-			<StationPoint
-				cx="1728.824"
-				cy="278.344"
-				label="Pankow"
-			/>
-			<StationPoint
-				cx="1348.721"
-				cy="1017.5"
-				label="Potsdamer Platz"
-			/>
+			{stations}
 		</>
 	);
 }
