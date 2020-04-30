68a2985916fe33358acb5c0e5688431b6778d43a
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-04-30 19:58:22 +0300

    Extracted U3 stations data to array of objects

--- src/Lines/U3Lines.js
+++ src/Lines/U3Lines.js
@@ -2,6 +2,71 @@ import React from "react";
 import StationPoint from "../StationPoint";
 
 export default function U3Lines() {
+	const data = [
+		{
+			cx: "378.0595",
+			cy: "1724.582",
+			label: "Krumme Lanke"
+		},
+		{
+			cx: "413.415",
+			cy: "1689.227",
+			label: "Onkel Toms Hütte"
+		},
+		{
+			cx: "448.77",
+			cy: "1653.868",
+			label: "Oskar-Helene-Heim"
+		},
+		{
+			cx: "484.125",
+			cy: "1618.516",
+			label: "Thielplatz"
+		},
+		{
+			cx: "519.48",
+			cy: "1583.157",
+			label: "Dahlem-Dorf"
+		},
+		{
+			cx: "554.836",
+			cy: "1547.805",
+			label: "Podbielskallee"
+		},
+		{
+			cx: "590.192",
+			cy: "1512.448",
+			label: "Breitenbachplatz"
+		},
+		{
+			cx: "625.547",
+			cy: "1477.094",
+			label: "Rüdesheimer Platz"
+		},
+		{
+			cx: "667.974",
+			cy: "1434.666",
+			label: "Heidelberger Platz"
+		},
+		{
+			cx: "813.914",
+			cy: "1290.297",
+			label: "Hohenzollernplatz"
+		},
+		{
+			cx: "910.105",
+			cy: "1194.105",
+			label: "Augsburger Straße"
+		}
+	];
+	const stations = data.map(station => (
+		<StationPoint
+			cx={station.cx}
+			cy={station.cy}
+			label={station.label}
+		/>
+	));
+
 	return (
 		<>
 			<polyline
@@ -12,61 +77,7 @@ export default function U3Lines() {
 				points="1097.695,1156.516 947.695,1156.516 
 	378.844,1725.365 "
 			/>
-			<StationPoint
-				cx="378.0595"
-				cy="1724.582"
-				label="Krumme Lanke"
-			/>
-			<StationPoint
-				cx="413.415"
-				cy="1689.227"
-				label="Onkel Toms Hütte"
-			/>
-			<StationPoint
-				cx="448.77"
-				cy="1653.868"
-				label="Oskar-Helene-Heim"
-			/>
-			<StationPoint
-				cx="484.125"
-				cy="1618.516"
-				label="Thielplatz"
-			/>
-			<StationPoint
-				cx="519.48"
-				cy="1583.157"
-				label="Dahlem-Dorf"
-			/>
-			<StationPoint
-				cx="554.836"
-				cy="1547.805"
-				label="Podbielskallee"
-			/>
-			<StationPoint
-				cx="590.192"
-				cy="1512.448"
-				label="Breitenbachplatz"
-			/>
-			<StationPoint
-				cx="625.547"
-				cy="1477.094"
-				label="Rüdesheimer Platz"
-			/>
-			<StationPoint
-				cx="667.974"
-				cy="1434.666"
-				label="Heidelberger Platz"
-			/>
-			<StationPoint
-				cx="813.914"
-				cy="1290.297"
-				label="Hohenzollernplatz"
-			/>
-			<StationPoint
-				cx="910.105"
-				cy="1194.105"
-				label="Augsburger Straße"
-			/>
+			{stations}
 		</>
 	);
 }
