db9dfff6e05c88cf7d061ec45f7986dac21f2b1c
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-04-30 20:02:22 +0300

    Extracted U4 stations data to array of objects

--- src/Lines/U4Lines.js
+++ src/Lines/U4Lines.js
@@ -2,6 +2,31 @@ import React from "react";
 import StationPoint from "../StationPoint";
 
 export default function U4Lines() {
+	const data = [
+		{
+			cx: "1037.695",
+			cy: "1263.332",
+			label: "Viktoria-Luise-Platz"
+		},
+		{
+			cx: "1037.695",
+			cy: "1401.998",
+			label: "Rathaus Schöneberg"
+		},
+		{
+			cx: "1037.695",
+			cy: "1447.043",
+			label: "Innsbrucker Platz"
+		}
+	];
+	const stations = data.map(station => (
+		<StationPoint
+			cx={station.cx}
+			cy={station.cy}
+			label={station.label}
+		/>
+	));
+
 	return (
 		<>
 			<polyline
@@ -12,21 +37,7 @@ export default function U4Lines() {
 				points="1097.695,1182.516 1037.695,1182.516 
 	1037.695,1447.031 "
 			/>
-			<StationPoint
-				cx="1037.695"
-				cy="1263.332"
-				label="Viktoria-Luise-Platz"
-			/>
-			<StationPoint
-				cx="1037.695"
-				cy="1401.998"
-				label="Rathaus Schöneberg"
-			/>
-			<StationPoint
-				cx="1037.695"
-				cy="1447.043"
-				label="Innsbrucker Platz"
-			/>
+			{stations}
 		</>
 	);
 }
