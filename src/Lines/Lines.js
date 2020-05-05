b830184f92eec58ad80bb10192acf0837fe6bf90
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-05-05 23:54:23 +0300

    Draw new lines for U3

--- src/Lines/Lines.js
+++ src/Lines/Lines.js
@@ -1,53 +1,53 @@
-import React from 'react';
+import React from "react";
+import U3Connections from "./U3/U3Connections";
+import { Station } from "../Station";
+import { connect } from "react-redux";
+import { Connection } from "./Connection";
 
-import { Station } from '../Station';
-import { connect } from 'react-redux';
-import { Connection } from './Connection';
-
-const u1Connections = require('./U1/connections.json');
-const u1Stations = require('./U1/stations.json');
+const u1Connections = require("./U1/connections.json");
+const u1Stations = require("./U1/stations.json");
+const u3Stations = require("./U3/stations.json");
 
 export function Lines({ connections, stations }) {
-  const drawnConnections = [...u1Connections].map((connection) => (
-    <Connection
-      stroke={connection.color}
-      key={connection.id}
-      x1={connection.x1}
-      y1={connection.y1}
-      x2={connection.x2}
-      y2={connection.y2}
-      stationA={connection.stationA}
-      stationB={connection.stationB}
-      route={[]}
-    />
-  ));
+	const drawnConnections = [...u1Connections].map(connection => (
+		<Connection
+			stroke={connection.color}
+			key={connection.id}
+			x1={connection.x1}
+			y1={connection.y1}
+			x2={connection.x2}
+			y2={connection.y2}
+			stationA={connection.stationA}
+			stationB={connection.stationB}
+			route={[]}
+		/>
+	));
 
-  const drawnStations = [
-    ...u1Stations,
-  ].map((station) => (
-    <Station
-      key={station.id}
-      id={station.id}
-      cx={station.cx}
-      cy={station.cy}
-      label={station.label}
-      transform={station.transform}
-    />
-  ));
+	const drawnStations = [...u1Stations, ...u3Stations].map(station => (
+		<Station
+			key={station.id}
+			id={station.id}
+			cx={station.cx}
+			cy={station.cy}
+			label={station.label}
+			transform={station.transform}
+		/>
+	));
 
-  return (
-    <>
-      {drawnConnections}
-      {drawnStations}
-    </>
-  );
+	return (
+		<>
+			<U3Connections />
+			{drawnConnections}
+			{drawnStations}
+		</>
+	);
 }
 
-const mapStateToProps = (state) => {
-  return {
-    stations: state.route.stations,
-    connections: state.route.connections,
-  };
+const mapStateToProps = state => {
+	return {
+		stations: state.route.stations,
+		connections: state.route.connections
+	};
 };
 
 const ConnectedLines = connect(mapStateToProps)(Lines);
--- src/Lines/U2/U2Connections.js
+++ src/Lines/U2/U2Connections.js
@@ -19,13 +19,13 @@ export default function U2Connections() {
   return (
     <>
       {connections}
-      {/* <polyline
+      <polyline
         fill='none'
         stroke='#E02021'
         stroke-width='8'
         stroke-miterlimit='10'
         points='1728.824,278.344 1728.824,958.396 1668.721,1018.5 1308.72,1018.805 1308.72,1098.805 1238.01,1169.516 987.695,1169.516 783.5,965.239 431,965.239 324.934,859.173'
-      /> */}
+      />
     </>
   );
 }
