0765f6d5158dfe87af9609b47e61f3e4421a1922
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-05-06 21:13:14 +0300

    Add missing U3 stations

--- src/Lines/Lines.js
+++ src/Lines/Lines.js
@@ -1,53 +1,35 @@
-import React from "react";
-import U3Connections from "./U3/U3Connections";
-import { Station } from "../Station";
-import { connect } from "react-redux";
-import { Connection } from "./Connection";
+import React from 'react';
+import U3Connections from './U3/U3Connections';
+import { Station } from '../Station';
+import { connect } from 'react-redux';
 
-const u1Connections = require("./U1/connections.json");
-const u1Stations = require("./U1/stations.json");
-const u3Stations = require("./U3/stations.json");
+const u3Stations = require('./U3/stations.json');
 
 export function Lines({ connections, stations }) {
-	const drawnConnections = [...u1Connections].map(connection => (
-		<Connection
-			stroke={connection.color}
-			key={connection.id}
-			x1={connection.x1}
-			y1={connection.y1}
-			x2={connection.x2}
-			y2={connection.y2}
-			stationA={connection.stationA}
-			stationB={connection.stationB}
-			route={[]}
-		/>
-	));
+  const drawnStations = [...u3Stations].map((station) => (
+    <Station
+      key={station.id}
+      id={station.id}
+      cx={station.cx}
+      cy={station.cy}
+      label={station.label}
+      transform={station.transform}
+    />
+  ));
 
-	const drawnStations = [...u1Stations, ...u3Stations].map(station => (
-		<Station
-			key={station.id}
-			id={station.id}
-			cx={station.cx}
-			cy={station.cy}
-			label={station.label}
-			transform={station.transform}
-		/>
-	));
-
-	return (
-		<>
-			<U3Connections />
-			{drawnConnections}
-			{drawnStations}
-		</>
-	);
+  return (
+    <>
+      <U3Connections />
+      {drawnStations}
+    </>
+  );
 }
 
-const mapStateToProps = state => {
-	return {
-		stations: state.route.stations,
-		connections: state.route.connections
-	};
+const mapStateToProps = (state) => {
+  return {
+    stations: state.route.stations,
+    connections: state.route.connections,
+  };
 };
 
 const ConnectedLines = connect(mapStateToProps)(Lines);
