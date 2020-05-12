7ad440cef688e3c30b63650155b3d70c7f54a925
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-05-12 12:02:55 +0300

    Add missing stations to U7

--- src/Lines/Lines.js
+++ src/Lines/Lines.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Station } from '../Station';
 import { connect } from 'react-redux';
-import U6Connections from './U6/U6Connections';
 import U7Connections from './U7/U7Connections';
 
-const u6Stations = require('./U6/stations.json');
+const u7Stations = require('./U7/stations.json');
 
 export function Lines({ connections, stations }) {
-  const drawnStations = [...u6Stations].map((station) => (
+  const drawnStations = [...u7Stations].map((station) => (
     <Station
       key={station.id}
       id={station.id}
@@ -20,7 +19,6 @@ export function Lines({ connections, stations }) {
 
   return (
     <>
-      <U6Connections />
       <U7Connections />
       {drawnStations}
     </>
