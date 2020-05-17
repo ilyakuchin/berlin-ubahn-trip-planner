4e0ac4458e4a25514dc16ade36fa7bb43dfd8f58
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-05-17 10:17:28 +0300

    Add configuration for travisCI

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,13 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store =
+  process.env.REACT_APP_MODE === 'dev'
+    ? createStore(
+        rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    : createStore(rootReducer);
 
 export default store;
